be52c993cedd89b5f1b6724176258b36eaf9ee15
AdamMar102003 <MARTINADAM2021@GMAIL.COM>
2026-02-27 10:26:22 -0500

    Enemies have Health
    Enemies should now have health.

--- Assets/BoxerAIEnemy.cs
+++ Assets/BoxerAIEnemy.cs
@@ -1,16 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxerAIEnemy : MonoBehaviour
 {
+    public float stamina, speed, damage, percentKO;
+    Vector3 boxerPos;
+
+    public GameObject leftGlove, rightGlove;
+    [SerializeField] private Transform lG, rG;
+    public GameObject Player;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        stamina = 100;
+        speed = 10f;
+        damage = 10;
+        percentKO = 0.1f;
+        boxerPos = transform.position;
+        leftGlove = lG.gameObject;
+        rightGlove = rG.gameObject;
+        Player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    public void GetPunched(float p)
+    {
+        stamina -= p;
+    }
+
+    public void Punch(GameObject fist)
+    {
+        fist.GetComponent<Rigidbody>().MovePosition(Player.GetComponent<Transform>().position);
+        //fist.GetComponent<Rigidbody>().MovePosition(Player.GetComponent<Transform>().position);
+    }
+
+    public void dodge(float dir)
+    {
+       rb.MovePosition(Player.GetComponent<Transform>().position);
+    }
+
+    public IEnumerator Wait(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(stamina <= 0)
+        {
+            Wait(2f);
+            Destroy(this.gameObject);
+        }
+
     }
 }
